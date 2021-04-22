8a275e64ec42cd550d4ff679440ab6d29baa52de
Leonardo Gonzalez Gutierrez <gonzalezgutierrezleonardo1@gmail.com>
2021-04-22 16:15:40 -0500

    Conexion por sockets, organizar frontend

---
+++ Server/index.js
@@ -0,0 +1,51 @@
+const SerialPort = require('serialport');
+const path = require('path');
+const express = require('express');
+const http = require('http');
+
+
+
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server, { cors: { origing: "*" } });
+
+// static files
+//app.use(express.static(path.join(__dirname, 'public')));
+
+const mySerial = new SerialPort(
+    'COM4', {
+        baudRate: 2000000,
+        autoOpen: true
+    });
+// routes
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+    console.log("a new socket connected" + socket.id);
+})
+
+
+const parser = new SerialPort.parsers.Readline()
+mySerial.pipe(parser);
+
+mySerial.on('open', function() {
+    console.log('Open serial port');
+});
+
+mySerial.on('data', function(line) {
+    console.log(line.toString());
+    io.emit('arduino:data', {
+        value: JSON.stringify("{" + line.toString() + "}") ///<<<<<<-----------------PENDIENTE POR PASAR A json
+    });
+});
+
+mySerial.on('error', function(err) {
+    console.log(err.message);
+
+});
+
+server.listen(3000, () => {
+    console.log('Server on port 3000');
+});
